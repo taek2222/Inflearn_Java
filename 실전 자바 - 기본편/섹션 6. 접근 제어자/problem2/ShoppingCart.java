1e9d2c22ac73ff0dadfd931f4022b5d1f0751412
taek2222 <oot309@daum.net>
2024-07-01 23:10:23 +0900

    feat: [섹션 6. 접근 제어자] 문제 풀이

---
+++ 실전 자바 - 기본편/섹션 6. 접근 제어자/problem2/ShoppingCart.java
@@ -0,0 +1,30 @@
+package problem2;
+
+public class ShoppingCart {
+    private Item[] items = new Item[10];
+    private int itemCount = 0;
+
+    public void addItem(Item item) {
+        if(itemCount == 10) {
+            System.out.println("장바구니가 가득 찼습니다.");
+            return;
+        }
+
+        items[itemCount] = item;
+        itemCount++;
+    }
+
+    public void displayItems() {
+        int total = 0;
+
+        System.out.println("장바구니 상품 출력");
+
+        for(int i = 0; i < itemCount; i++) {
+            int itemPrice = items[i].getTotalPrice();
+            System.out.println("상품명:" + items[i].getName() + ", 합계:" + itemPrice);
+            total += itemPrice;
+        }
+
+        System.out.println("전체 가격 합:" + total);
+    }
+}
