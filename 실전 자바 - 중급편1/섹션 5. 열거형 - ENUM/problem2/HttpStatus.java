d05b0b25c8d09bca6cee7c2bd8ae903b17e24a67
taek2222 <oot309@daum.net>
2024-07-11 16:08:11 +0900

    feat: [섹션 5. 열거형] 문제 풀이

---
+++ 실전 자바 - 중급편1/섹션 5. 열거형 - ENUM/problem2/HttpStatus.java
@@ -0,0 +1,37 @@
+package problem2;
+
+enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static HttpStatus findByCode(int httpCodeInput) {
+        HttpStatus[] values = HttpStatus.values();
+
+        for (HttpStatus value : values)
+            if(value.code == httpCodeInput)
+                return value;
+        return null;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSuccess() {
+        return code >= 200 && code <= 299;
+    }
+}
